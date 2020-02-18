f1a3d3f186b2ec37d4effe8bc3d7cdfca5a9acf2
MasayaYamada <masaya.yamada.stripes@gmail.com>
2020-02-18 13:49:42 +0900

    Initial Commit

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by Yamada, Masaya on 2/18/20.
+//  Copyright © 2020 Yamada, Masaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
