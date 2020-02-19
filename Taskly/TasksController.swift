4ec77127faba266352e0b379183e40a3845ac604
MasayaYamada <masaya.yamada.stripes@gmail.com>
2020-02-19 17:00:07 +0900

    processing action

--- Taskly/TasksController.swift
+++ Taskly/TasksController.swift
@@ -21,7 +21,24 @@ class TasksController: UITableViewController {
         let alertController = UIAlertController(title: "Add Task", message: nil, preferredStyle: .alert)
         
         // Set up the actions
-        let addAction = UIAlertAction(title: "Add Task", style: .default, handler: nil)
+        let addAction = UIAlertAction(title: "Add Task", style: .default) { _ in
+        
+            // Grab text field  text
+            guard let name = alertController.textFields?.first?.text else { return }
+            
+            // Create task
+            let newTask = Task(name: name)
+            
+            // Add task
+            self.taskStore.add(newTask, at: 0)
+            
+            // Reload data in table view
+            let indexPath = IndexPath(row: 0, section: 0)
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+            
+        }
+        
+        
         addAction.isEnabled = false
         
         let cancelAction = UIAlertAction(title: "Canncel", style: .cancel)
@@ -46,6 +63,8 @@ class TasksController: UITableViewController {
     @objc private func handleTextChanged(_ sender: UITextField) {
         
         // Grab the alert controller and add action
+        //Guards are so so helpful and I use them all the time because it won't allow any code below the guard
+        //statement to be executed unless certain conditions are met.
         guard let alertController = presentedViewController as? UIAlertController,
               let addAction = alertController.actions.first,
               let text = sender.text
@@ -62,10 +81,6 @@ class TasksController: UITableViewController {
 //MARK: - DataSource
 extension TasksController {
     
-    // sectionのヘッダーの高さを調整
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 54
-    }
     // sectionのヘッダータイトルを調整
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return section == 0 ? "To-Do" : "Done"
@@ -88,4 +103,68 @@ extension TasksController {
         return cell
     }
 
+}
+
+// MARK: - Delegate
+extension TasksController {
+    // sectionのヘッダーの高さを調整
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 54
+    }
+    
+    // TableViewのスワイプ時の効果 right swipe
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) { (action, sourceView, completionHandler) in
+            
+            // Determine whether the task `isDone`
+            let isDone = self.taskStore.tasks[indexPath.section][indexPath.row].isDone
+            
+            // Remove the task from the appropriate array
+            self.taskStore.removeTask(at: indexPath.row, isDone: isDone)
+            
+            // Reload table view
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            // Indicate that the action was performed
+            completionHandler(true)
+        }
+        
+        deleteAction.image = UIImage(named: "delete")
+        deleteAction.backgroundColor = #colorLiteral(red: 0.8862745098, green: 0.1450980392, blue: 0.168627451, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    //left swipe
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let doneAction = UIContextualAction(style: .normal, title: nil) {
+            (action, sourceView, completionHandler) in
+            
+            // Toggle that the task is done
+            self.taskStore.tasks[0][indexPath.row].isDone = true
+            
+            // Remove the task from the array containing todo tasks
+            let doneTask = self.taskStore.removeTask(at: indexPath.row)
+            
+            // add the task from the array containing done tasks
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            // Reload table view
+            self.taskStore.add(doneTask, at: 0, isDone: true)
+            
+            // Indicate the action was performed
+            tableView.insertRows(at: [IndexPath(row: 0, section: 1)], with: .automatic)
+            
+            completionHandler(true)
+            
+        }
+        
+        doneAction.image = UIImage(named: "done")
+        doneAction.backgroundColor = .green
+        
+        return indexPath.section == 0 ? UISwipeActionsConfiguration(actions: [doneAction]) : nil
+        
+    }
 }
